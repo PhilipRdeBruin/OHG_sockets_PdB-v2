6b39f9cb47a428de36933e248d7b90346acb6f75
Hergen Dillema <hergen.dillema@gmail.com>
2019-01-15 10:30:51 +0100

    game and player setup based on per game config

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var app = require('express')();
 var mysql = require('mysql');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var gamesAvailable = ['bke'];
+// Games Available Template = Game Name: [Player Count, Initial Game State]
+var gamesAvailable = { bke: [2, [[0,0,0],[0,0,0],[0,0,0]]], example: []};
 var globalGameState = [];
 var db = mysql.createConnection({
     host: 'localhost',
@@ -23,40 +24,44 @@ io.on('connection', function(socket){
     socket.on('join room', function(gData){
         connectedUsers[gData.user] = socket.id;
         var room = gData.game + gData.room;
+        var playerAmount = gamesAvailable[gData.game][0];
         socket.join(room);
         console.log("room: " + gData.room);
-        console.log('game: ' + gData.game);
+        console.log(gamesAvailable[gData.game]);
         socket["username"] = gData.user;
 
-        // LOAD INITIAL GAMSTATE FROM DATABASE
+        // SET UP INITIAL GAMESTATE
         if(globalGameState[room] == null){
             globalGameState[room] = [];
             globalGameState[room]['users'] = [];
-            globalGameState[room]['gamestate'] = [[0,0,0],[0,0,0],[0,0,0]]
+            globalGameState[room]['gamestate'] = gamesAvailable[gData.game][1];
             console.log("init: " + globalGameState[room]['gamestate']);
             console.log("test: " + globalGameState[room]['gamestate'][1][1])
+        } else if(globalGameState[room]["active"] != null){
+                var sendTo = connectedUsers[globalGameState[room]["users"][globalGameState[room]["active"]]];
+                console.log(sendTo);
+                io.to(`${sendTo}`).emit('game turn', 1);
         }
-       if(globalGameState[room]['users'].length < 2) {
-           if(globalGameState[room]['users'].indexOf(gData.user) < 0) {
+        // SET UP INITIAL USERS
+        if(globalGameState[room]['users'].length < playerAmount) {
+            if(globalGameState[room]['users'].indexOf(gData.user) < 0) {
                console.log("user not found, adding: " + gData.user);
                globalGameState[room]['users'].push(gData.user);
             } else {
             console.log("continuing with users: " + globalGameState[room]['users'])
             }
-            if (globalGameState[room]['users'].length == 2){
-                globalGameState[room]["active"] = randomUser(2);
+            if (globalGameState[room]['users'].length == playerAmount){
+                globalGameState[room]["active"] = randomUser(playerAmount);
                 console.log(globalGameState[room]["active"]);
                 console.log("full! starting game with users: " + globalGameState[room]['users']);
                 var sendTo = connectedUsers[globalGameState[room]["users"][globalGameState[room]["active"]]];
                 console.log(sendTo);
                 io.to(`${sendTo}`).emit('game turn', 1);
             }
-        } else {
-           console.log(gData.user + " added as spectator")
-       }
-
-        // 
+        }
     });
+
+    // PROCESS GAME MOVE
     socket.on('game move', function(move){
         room = Object.keys(io.sockets.adapter.sids[socket.id])[1];
         if(globalGameState[room]["users"][globalGameState[room]["active"]] == socket.username) {
