8f7221c201271558ccee6162dfad5dcf7caf5f4a
Hergen Dillema <hergen.dillema@gmail.com>
2019-01-18 11:25:56 +0100

    minor

--- rules_mastermind.js
+++ rules_mastermind.js
@@ -14,6 +14,7 @@ exports.gameMove = function(globalGameState, room, move) {
     // globalGameState[room]["result"] = winCon(gamestate, currentPLayer);
 
     // SUBMIT new gamestate
+    
     gamestate = JSON.stringify(gamestate);
     globalGameState[room]["gamestate"] = gamestate;
 }
